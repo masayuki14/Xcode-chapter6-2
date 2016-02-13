a451207ff67109f3ac99da3dfbbe0493b3cf1c51
Masayuki Morisaki <masayuki144@gmail.com>
2016-02-13 14:38:27 +0900

    Initial Commit

---
+++ chapter6-2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  chapter6-2
+//
+//  Created by 森崎 雅之 on 2016/02/13.
+//  Copyright © 2016年 森崎 雅之. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ chapter6-2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  chapter6-2
+//
+//  Created by 森崎 雅之 on 2016/02/13.
+//  Copyright © 2016年 森崎 雅之. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
